a2b3d07ec41b12ddaaf7904e1ee05fb4f870c672
Benjamin Goering <ben@bengo.co>
2018-06-08 23:43:15 +0200

    Remove unused things in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-import {
-  ASObject,
-  ASObjectType,
-  ASValue,
-  LDValue,
-  xsdAnyUri,
-} from "./core"
-
 export const jsonLdProfile = "https://www.w3.org/ns/activitystreams"
+
 // application/ld+json; profile="https://www.w3.org/ns/activitystreams"
 export const ASJsonLdProfileContentType = `application/ld+json; profile="${jsonLdProfile}"`
-
-export class JSONLD {
-  public "@id": string;
-}
