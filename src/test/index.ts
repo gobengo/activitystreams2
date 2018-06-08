967954bd8939c0f8ffa5f968188ad1c22ff4bbdc
Benjamin Goering <ben@bengo.co>
2018-06-08 17:27:24 +0200

    initial tslint, tsconfig, and src/index.ts

---
+++ src/test/index.ts
@@ -0,0 +1,12 @@
+import * as assert from "assert"
+import * as activitystreams2 from "../"
+
+if (require.main === module) {
+    main()
+    .then(() => process.exit())
+    .catch(() => process.exit(1))
+}
+
+async function main() {
+    assert.equal(typeof activitystreams2.ASJsonLdProfileContentType, "string")
+}
