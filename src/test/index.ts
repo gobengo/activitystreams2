53aba5ba31cc2d7d247bce8bc7649288c98175bc
Benjamin Goering <ben@bengo.co>
2018-06-09 20:52:55 +0200

    Add dev dep of activitystreams2-spec-scraped and test that we have all activityTypes

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,10 +1,22 @@
 import * as assert from 'assert';
 import {jsonLdProfileContentType} from '../core';
+import * as vocab from "../vocabulary";
+import { scrapeVocabulary } from "activitystreams2-spec-scraped";
 
 if (require.main === module) {
-  main().then(() => process.exit()).catch(() => process.exit(1));
+  main()
+  .then(() => process.exit())
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  });
 }
 
 async function main() {
   assert.equal(typeof jsonLdProfileContentType, 'string');
+  const scrapedVocab = await scrapeVocabulary()
+  console.log(scrapedVocab)
+  for (const activityType of scrapedVocab.activityTypes) {
+    assert.equal(activityType.name in vocab, true, `vocab exports activityType ${activityType.name}`)
+  }
 }
